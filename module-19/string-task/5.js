6f48bc065aec7e13fc7a8fbdc1ed885e2bbe6e01
mus03 <agarwallamuskan04@gmail.com>
2024-01-31 21:48:03 +0600

    module-19 added

--- module-19/string-task/5.js
+++ module-19/string-task/5.js
@@ -1,10 +1,11 @@
 // Capitalize Every first Letter of each word in a String
 let str = "my name is muskan";
-str = str.split(" ");
-console.log(str);
-for (let i = 0; i < str.length; i++) {
-  console.log(str[i]);
-  if (i === 0 || str[i] === " ") {
-    console.log(str[i].toUpperCase());
-  }
+str2 = str.split(" ");
+console.log(str2);
+for (let i = 0; i < str2.length; i++) {
+  // console.log(str[i]);
+  // console.log(str[i][0].toUpperCase() + str[i]);
+  str2[i] = str2[i][0].toUpperCase() + str2[i].slice(1);
 }
+console.log(str2);
+console.log(str2.join(" "));
