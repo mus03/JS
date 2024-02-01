ea17b7cdd2d65c078de2e42f2ee184e2d8db4a34
mus03 <agarwallamuskan04@gmail.com>
2024-02-01 21:14:42 +0600

    module 19.5 added

--- module-19.5/3.js
+++ module-19.5/3.js
@@ -5,13 +5,13 @@
 // 'TomTimTinTik'
 var numbers = ["Tom", "Tim", "Tin", "Tik"];
 console.log(numbers);
-let result = [];
+let result = "";
 for (const number of numbers) {
   console.log(number);
   result = result.concat(number); //string added to an empty array and store the strings in that empty array
 }
 console.log(result); //[ 'Tom', 'Tim', 'Tin', 'Tik' ] (array)
-console.log(result.join("")); // TomTimTinTik (string)
+// console.log(result.join("")); // TomTimTinTik (string)
 
 // --------------------------------------------------------------------------------
 // var numbers = ["Tom", "Tim", "Tin", "Tik"];
