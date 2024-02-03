26e17c1af52190322a9abe137bb80e3b76a0c416
mus03 <agarwallamuskan04@gmail.com>
2024-02-03 19:22:24 +0600

    commit

--- module-18/break/3.js
+++ module-18/break/3.js
@@ -2,10 +2,12 @@
 Write a loop that goes from 1 to 100, 
 but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
-for (let i = 1; i <= 100; i++) {
-  let square = i * i;
-  console.log("value = ", i, ", square = ", square);
-  if (square <= 100) {
+for (let i = 2; i <= 100; i++) {
+  // let square = i * i;
+  // console.log("value = ", i);
+  let a = Math.sqrt(i);
+  console.log("a:", a);
+  if (a === 2) {
     break;
   }
   console.log(i);
--- module-19/string-task/4.js
+++ module-19/string-task/4.js
@@ -1,17 +1,29 @@
 // If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 // Hint: You should be able to check whether x or X exists. After that,
 // search online how to replace a character in a string.
-let str = "maxMAX";
+// let str = "maxMAX";
 // let strArray = str.split(" "); // array te split kaaj kore nh
 // console.log(strArray);
-for (let i = 0; i <= str.length; i++) {
-  //   console.log(str[i]);
-  if (str[i] === "x" || str[i] === "X") {
-    if (str[i] === "x") {
-      str = str.replace(str[i], "y");
-    } else if (str[i] === "X") {
-      str = str.replace(str[i], "Y");
-    }
+// for (let i = 0; i <= str.length; i++) {
+//   //   console.log(str[i]);
+//   if (str[i] === "x" || str[i] === "X") {
+//     if (str[i] === "x") {
+//       str = str.replace(str[i], "y");
+//     } else if (str[i] === "X") {
+//       str = str.replace(str[i], "Y");
+//     }
+//   }
+// }
+// console.log(str);
+let str =
+  "If a given string has either x, replace x by y. if the given string has X, replace it by Y. i have a Xmax tree";
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+  if (str[i].includes("x")) {
+    str = str.replace(str[i], "y");
+  }
+  if (str[i].includes("X")) {
+    str = str.replace(str[i], "Y");
   }
 }
 console.log(str);
